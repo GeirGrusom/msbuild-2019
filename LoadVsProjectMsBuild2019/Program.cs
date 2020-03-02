040fef0386b82ca95476b35fd71a4853ea747b14
Henning Moe <henning.moe@kdi.kongsberg.com>
2020-03-02 10:25:19 +0100

    Initial commit

---
+++ LoadVsProjectMsBuild2019/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LoadVsProjectMsBuild2019
+{
+    class Program
+    {
+        static int Main()
+        {
+            return LoadProject("NewProjectFormat.csproj") | LoadProject("OldProjectFormat.csproj");
+        }
+
+        static int LoadProject(string filename)
+        {
+            try
+            {
+                var proj = Microsoft.Build.Evaluation.Project.FromFile(filename, new Microsoft.Build.Definition.ProjectOptions());
+                Console.WriteLine(proj.GetProperty("Message").EvaluatedValue);
+                return 0;
+            }
+            catch (Microsoft.Build.Exceptions.InvalidProjectFileException invex)
+            {
+                Console.WriteLine(invex.Message);
+                return 1;
+            }
+        }
+    }
+}
